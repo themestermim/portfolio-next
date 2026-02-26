8f80abb4d0fe6d29fab2b343f5c45feefe53ab80
Mehran Mahmoudi Arasb <themestermim@gmail.com>
2026-02-26 10:50:50 +0330

    favicon added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,34 @@ import Header from "@/components/Header/header";
 import SharedSvgs from "@/components/Svgs/Shared";
 
 export const metadata: Metadata = {
-  title: 'مهران محمودی - برنامه نویس ارشد',
-  description: 'با بیش از 5 سال تجربه در توسعه و بازطراحی اپلیکیشن‌های وب مقیاس‌پذیر.',
+    title: 'مهران محمودی - برنامه نویس ارشد',
+    description: 'با بیش از 5 سال تجربه در توسعه و بازطراحی اپلیکیشن‌های وب مقیاس‌پذیر.',
+    icons: {
+        icon: [
+            { url: '/favicon.svg' },
+            { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+            { url: '/favicon.svg', type: 'image/svg+xml' },
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png', sizes: '180x180' },
+        ],
+    },
+    manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="fa">
-      {/* className='pt-12' */}
-      <body>
-        <SharedSvgs />
-        <Header />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="fa">
+            {/* className='pt-12' */}
+            <body>
+                <SharedSvgs />
+                <Header />
+                {children}
+            </body>
+        </html>
+    )
 }
